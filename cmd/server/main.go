e6069db118d6d10e2cd8fbfed3aaa85f4998fc59
Patrick Callahan <patricktcallahan18@gmail.com>
2026-04-19 18:20:45 +0200

    Implement RabbitMQ connection and signal handling in main.go; update go.mod and go.sum for dependencies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connAddr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connAddr)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down...")
 }
